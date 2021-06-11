7576a6d4208951da24f90d1fd10221a9f9843bf1
Kellen Presta <kpresta04@gmail.com>
2021-06-11 13:50:14 +0100

    add restaurant components

---
+++ src/components/RestaurantList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Restaurant List</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/RestaurantScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Rscreen
+
+    <restaurant-list />
+  </div>
+</template>
+
+<script>
+import RestaurantList from './RestaurantList.vue';
+export default {
+  components: {RestaurantList},
+};
+</script>
+
+<style lang="scss" scoped></style>
