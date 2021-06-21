8aa28a69ec99b4dd0e8e3cbbcbce53e7bd2af41b
Kellen Presta <kpresta04@gmail.com>
2021-06-21 15:30:16 +0100

    check one behavior per unit test

---
+++ tests/unit/components/RestaurantList.spec.js
@@ -0,0 +1,54 @@
+import Vuex from 'vuex';
+import {mount, createLocalVue} from '@vue/test-utils';
+import RestaurantList from '@/components/RestaurantList';
+
+describe('RestaurantList', () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  it('loads restaurants on mount', () => {
+    const restaurantsModule = {
+      namespaced: true,
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    mount(RestaurantList, {localVue, store});
+
+    expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    const records = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    const restaurantsModule = {
+      namespaced: true,
+      state: {records},
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    const wrapper = mount(RestaurantList, {localVue, store});
+
+    const firstRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(0)
+      .text();
+    expect(firstRestaurantName).toBe('Sushi Place');
+  });
+});
